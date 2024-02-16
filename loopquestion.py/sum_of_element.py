b932c4334b94868e2bf0c978081c62ae2da76ed6
Vijay6923 <kumarviju2468@gmail.com>
2024-02-16 14:21:54 +0530

    maximum no of 1 in the row

---
+++ loopquestion.py/sum_of_element.py
@@ -0,0 +1,15 @@
+matrix = [[0, 0, 1, 1, 1],[0, 0, 0, 0, 1],[0, 1, 1, 1, 1],[0, 0, 0, 0, 1],[1, 1, 1, 1, 1]]
+
+max_ones = 0
+max_row = -1
+
+for i in range(len(matrix)):
+    count_ones = 0
+    for j in range(len(matrix[i])):
+        if matrix[i][j] == 1:
+            count_ones += 1
+    if count_ones > max_ones:
+        max_ones = count_ones
+        max_row = i
+
+print("Row with maximum number of 1s:", max_row + 1)
