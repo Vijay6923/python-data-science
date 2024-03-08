80b4878f3046f6b61409ded4569b19609e6cd694
Vijay6923 <kumarviju2468@gmail.com>
2024-03-09 02:10:29 +0530

    try-catch

--- loopquestion.py/iitguwahati/bool.py
+++ loopquestion.py/iitguwahati/bool.py
@@ -1,6 +1,13 @@
 # bool_true=True
 # print("type of true:",type(bool_true))
-name1="Alice"
-name2="Bob"
-is_equal=name1==name2
-print(is_equal)
+# name1="Alice"
+# name2="Bob"
+# is_equal=name1==name2
+# print(is_equal)
+try:
+    numerator=int(input("enter the numerator: "))
+    denominator=int(input("enter the denominator: "))
+    result=numerator/denominator
+    print(result)
+except ZeroDivisionError as zero_division_error:
+    print("Error:",zero_division_error)
