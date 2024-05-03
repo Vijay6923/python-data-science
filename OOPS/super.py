bbcb5f35c02f89c0ef3301df40cff62fe18181b0
Vijay6923 <kumarviju2468@gmail.com>
2024-05-03 22:20:09 +0530

    oops

--- OOPS/super.py
+++ OOPS/super.py
@@ -7,10 +7,10 @@ class Parent:
 
 class Child(Parent):
     def __init__ (self,name,age):
-        super()__init__ (name)
+        super() (name)
         self.age=age
     def display(self):
         super().display()
-        print("child class display method: "self.age)
+        print("child class display method: ",self.age)
 child=Child("Alice",10)
 child.display() 
